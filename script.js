904cf59fb4b730ada47f965559c90b6d6dd3a8b6
Nawaf <83686663+knas1@users.noreply.github.com>
2023-11-13 07:45:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const getReposButton = document.getElementById('get-repos');
-const reposList = document.getElementById('repos-list');
+const getReposButton = document.getElementById("get-repos");
+const reposList = document.getElementById("repos-list");
 
-getReposButton.addEventListener('click', async () => {
-    const username = document.getElementById('username').value;
-    /* Send an HTTP GET request to the GitHub /users/{username}/repos end point and 
+getReposButton.addEventListener("click", async () => {
+  const username = document.getElementById("username").value;
+  /* Send an HTTP GET request to the GitHub /users/{username}/repos end point and 
     display the list of repositories in the <ul> element provided in the HTML code.
     */
-    // Example: https://api.github.com/users/facebook/repos
-    const url = `https://api.github.com/users/${username}/repos`;
-    const ulElement = document.getElementById('repos-list');
-    // Fetch and display the list of repositories as li elements inside the ul element.
-    const response = await fetch(url);
-    const data = await response.json();
+  // Example: https://api.github.com/users/facebook/repos
+  const url = `https://api.github.com/users/${username}/repos`;
+  const ulElement = document.getElementById("repos-list");
+  // Fetch and display the list of repositories as li elements inside the ul element.
+  const response = await fetch(url);
+  const data = await response.json();
 
-    reposList.innerHTML='';
+  reposList.innerHTML = "";
+
+  data.forEach((repo) => {
+    const li = document.createElement("li");
+    li.innerText = repo.name;
+    reposList.appendChild(li);
+  });
 });
-
